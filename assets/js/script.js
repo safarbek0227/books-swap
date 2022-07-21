a7a312d790fe9fa8bb50d1e577780dd5a57ceae3
Safarbek <solijonovsafarbek@gmail.com>
2022-07-21 05:07:39 -0700

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,26 @@ $('html').click(function(e) {
     $('.dropdown.active').not($curr).removeClass('active');
   });
 
+
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    dots: false,
+    nav: false,
+  
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      1336: {
+        item: 4
+      },
+      1340: {
+        items: 4
+      }
+    }
+  })
+  
